396e2a0d222c7525a45f919793d90d671a463f79
Rohit <rohitparih@gmail.com>
2024-06-02 15:33:51 +0530

    ci/workflow-added

--- src/main/java/com/bloggios/blog/BlogProviderApplication.java
+++ src/main/java/com/bloggios/blog/BlogProviderApplication.java
@@ -2,12 +2,16 @@ package com.bloggios.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {
         "com.bloggios.blog",
         "com.bloggios.authenticationconfig",
         "com.bloggios.elasticsearch.configuration"
 })
+@EnableEurekaClient
+@EnableAsync
 public class BlogProviderApplication {
 
     public static void main(String[] args) {
